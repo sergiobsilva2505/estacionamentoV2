bd2a3f2d290b80d88b173f20ee1da80d8292abcf
Sergio Bezerra da Silva <sergio.bezerra@outlook.com>
2021-03-04 12:12:35 -0300

    Utilizando o Model

--- src/main/java/br/com/fcamara/trainee/estacionamento/controller/HelloController.java
+++ src/main/java/br/com/fcamara/trainee/estacionamento/controller/HelloController.java
@@ -1,13 +1,15 @@
 package br.com.fcamara.trainee.estacionamento.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 	
 	@GetMapping("/hello")
-	public String olaMundo() {
+	public String olaMundo(Model model) {
+		model.addAttribute("nome", "Olá Mundo!!!");
 		return "hello";
 	}
 
